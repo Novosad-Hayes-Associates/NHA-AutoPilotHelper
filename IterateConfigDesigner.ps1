72d660ad95cf9d98948c6fd03518392b41e8459d
Tin Can Coding <84742032+tincancodings@users.noreply.github.com>
2023-06-08 15:29:07 -0500

    Full Script, predebug

--- IterateConfigDesigner.ps1
+++ IterateConfigDesigner.ps1
@@ -1,25 +1,11 @@
 $curTime = get-date -Format "yyyyMMddHHss"
-if ( $env:COMPUTERNAME -eq 'LOONA' ) {
-    $varGitRoot = 'D:\Git'
-    $varRootPath = 'D:\VOD\OneDrive - Novosad Hayes Associates\ISO-Hive'
-    $varBuild = 'c:\build'
-    $varWorkhold = 'c:\WorkHold'
-    $varThreads = '128'
-    Write-Host "Welcome TinCanCoding"
-}elseif ( $env:COMPUTERNAME -eq 'JBRADEN-WK') {
-    $varGitRoot = 'F:\ISO-Scripts'
-    $varRootPath = 'F:\cburditt-onedrive\OneDrive - Novosad Hayes Associates\ISO-Hive'
-    $varBuild = 'F:\build'
-    $varWorkhold = 'F:\WorkHold'
-    $varThreads = '1'
-    Write-Host "Welcome user."
-}elseif ( $env:COMPUTERNAME -eq 'RPRATT-SURF') {
+if ( $env:COMPUTERNAME -eq 'RPRATT-SURF') {
     $varGitRoot = 'C:\Git'
-    $varRootPath = 'C:\users\rpratt\OneDrive - Novosad Hayes Associates\ISO-Hive'
-    $varBuild = 'c:\build'
-    $varWorkhold = 'c:\WorkHold'
-    $varThreads = '128'
+    $varConfigDesignSrc = "C:\Users\rpratt\OneDrive - Novosad Hayes Associates\Documents\Windows Imaging and Configuration Designer (WICD)"
+    $varConfigDesginDst = "C:\Users\rpratt\OneDrive - Novosad Hayes Associates\Documents\WICD-Backup\$curTime"
+    $varThreads = '1'
     Write-Host "Welcome TinCanCoding"
 }else {
     Write-Error "Don't recognize this system mate! Sort Your SHIT DUDE!"
 }
+Robocopy.exe $varConfigDesignSrc $varConfigDesginDst /MIR /MT:$varTreads
