f4ae01740e543a914d94b95f251569994e85b1b9
Tin Can Coding <84742032+tincancodings@users.noreply.github.com>
2023-06-08 15:32:03 -0500

    Trap computer, just incase

--- IterateConfigDesigner.ps1
+++ IterateConfigDesigner.ps1
@@ -1,11 +1,17 @@
 $curTime = get-date -Format "yyyyMMddHHss"
+$sentinal = 0
 if ( $env:COMPUTERNAME -eq 'RPRATT-SURF') {
     $varGitRoot = 'C:\Git'
     $varConfigDesignSrc = "C:\Users\rpratt\OneDrive - Novosad Hayes Associates\Documents\Windows Imaging and Configuration Designer (WICD)"
     $varConfigDesginDst = "C:\Users\rpratt\OneDrive - Novosad Hayes Associates\Documents\WICD-Backup\$curTime"
     $varThreads = '1'
     Write-Host "Welcome TinCanCoding"
-}else {
+    $sentinal = '1'
+} else {
     Write-Error "Don't recognize this system mate! Sort Your SHIT DUDE!"
 }
-Robocopy.exe $varConfigDesignSrc $varConfigDesginDst /MIR /MT:$varTreads
+if ($sentinal -eq '1') {
+    Robocopy.exe $varConfigDesignSrc $varConfigDesginDst /MIR /MT:$varTreads
+} else {
+    Write-Output "Yeah, sorry, can't process."
+}
