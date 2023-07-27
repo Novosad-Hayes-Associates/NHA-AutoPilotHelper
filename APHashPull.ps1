688bf5d01def19b64896c1b1df8d8c4880234543
Tin Can Coding <84742032+tincancodings@users.noreply.github.com>
2023-07-27 09:45:08 -0500

    Init APHashPull.ps1

---
+++ APHashPull.ps1
@@ -0,0 +1,11 @@
+$varLocalRemDrives = get-volume | where drivetype -eq removable | foreach driveletter
+if ($varLocalRemDrives.Count -eq '0') {
+    Write-Host "Put a drive in and we can do some work."
+} elseif ($varLocalRemDrives.Count -eq '1') {
+    $varDrive = $varLocalRemDrives
+} else {
+    Write-Output "You have too many drives, which one do you want?"
+    Write-Output $varLocalRemDrives
+    $varDrive = Read-Host "What Drive are we looking for?"
+}
+$varDrive += ':'
