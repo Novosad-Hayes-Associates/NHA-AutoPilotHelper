9ad1411a8fc3a26fec07093942e2bb388f6fc757
Tin Can Coding <84742032+tincancodings@users.noreply.github.com>
2023-08-04 11:48:41 -0500

    Last File Cleanup

--- HashPuller/Output/gap.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-echo Enable WimRM
-PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command Enable-PSRemoting -SkipNetworkProfileCheck -Force
-echo Getting Hash
-PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command %~dp0Get-WindowsAutoPilotInfo.ps1 -ComputerName $env:COMPUTERNAME -OutputFile %~dp0compHash.csv -append
-echo All done.
-pause
